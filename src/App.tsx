962ae6c6b928e298a10e436f0355503515b5b622
Sergej Garkusha <Tenebrann@googlemail.com>
2021-10-24 23:42:36 +0200

    add node sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Header } from './modules/components/Header';
+import { Blog } from './modules/pages/Blog/Blog';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      {/* <Header /> */}
+      <Blog />
     </div>
   );
 }
--- src/modules/pages/Blog/Blog.tsx
+++ src/modules/pages/Blog/Blog.tsx
@@ -1,8 +1,20 @@
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { BlogType, getBlogTC } from '../../../store/blog-reducers';
+import { AppRootStateType } from '../../../store/store';
+import { useDispatch, useSelector } from 'react-redux';
+import Paper from '@material-ui/core/Paper';
 
 export const Blog = () => {
+  const blog = useSelector<AppRootStateType, Array<BlogType>>(state => state.blog.blog);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBlogTC());
+  }, []);
+
+  console.log(blog);
+
   return (
     <Container fixed>
       {/* <Grid container className="add-todolist">
@@ -10,6 +22,15 @@ export const Blog = () => {
 </Grid> */}
 
       <Grid container spacing={5}>
+        {blog.map(blog => {
+          return (
+            <Grid item key={blog.id}>
+              <Paper elevation={5}>
+                <div>{blog.title}</div>
+              </Paper>
+            </Grid>
+          );
+        })}
         {/* {todolist.map(list => {
   return (
     <Grid item key={list.id}>
